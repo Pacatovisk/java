0fb42b1d9d729b2bfa8d0f2cad6464105148018d
marcelo.dias <marcelovianadias@outlook.com>
2021-02-18 20:19:21 -0300

    criando a classe funcionario

---
+++ src/poo/Funcionario.java
@@ -0,0 +1,31 @@
+package poo;
+
+public class Funcionario {
+    private String nome;
+    private int idade;
+    private String funcao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+}
